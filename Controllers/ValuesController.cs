628a5843c3740f23552bdf106b8dab9587bf10a5
Da Silva Seles <gsseles@indracompany.com>
2020-08-26 18:16:22 -0300

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgilAPI.Data;
+using ProAgilAPI.Model;
+
+namespace ProAgil.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly DataContext context;
+        public ValuesController(DataContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Evento>>> Get()
+        {
+            try
+            {
+                var results = await context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Evento>> Get(int id)
+        {
+            return await context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+        }
+    }
+}
+
---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProAgilAPI.Model;
+
+namespace ProAgilAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        readonly DbContextOptions options;
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            this.options = options;
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
